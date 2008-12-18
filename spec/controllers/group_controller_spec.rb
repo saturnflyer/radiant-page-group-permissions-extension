312228d97bbb6c54a8e08c12e2834bd526836a05
Jim Gay <jim@saturnflyer.com>
2008-12-19 07:08:09 +0800

    order groups by name
    Signed-off-by: Tim Gossett <tim@mrgossett.com>

---
+++ spec/controllers/group_controller_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Admin::GroupController do
+  scenario :users
+  
+  before do
+    @group_1 = Group.create!(:name => 'Z Group 1')
+    @group_2 = Group.create!(:name => 'Group 1')
+    @group_3 = Group.create!(:name => 'Group 2')
+    @groups = [@group_2, @group_3, @group_1]
+    login_as :admin
+  end
+  
+  describe "index with GET request" do
+    it "should order Groups by name" do
+      get :index
+      assigns(:groups).should == @groups
+    end
+  end
+
+end
