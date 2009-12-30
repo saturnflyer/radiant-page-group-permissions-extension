c9d05b79bb26b7a26b290f18c160d30432db959c
Andrew vonderLuft <avonderluft@avlux.net>
2009-12-31 07:53:53 +0800

    replace obsolete 'order_by' with 'named_scope'

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
-class Group < ActiveRecord::Base
-  order_by 'name'
+class Group < ActiveRecord::Base  
+  named_scope :default, :order => "name"
   
   belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by'
   belongs_to :updated_by, :class_name => 'User', :foreign_key => 'updated_by'
